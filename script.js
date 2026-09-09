cbe1102625d21c869c357f0c401cae7ac97dbe3a
Arman Alizadeh <armanalizadeh467@gmail.com>
2026-09-09 19:48:09 +0200

    feat: build responsive navigation and hero section

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+const menuBtn = document.querySelector(".menu__btn");
+const navLinks = document.querySelector(".header__nav-elements");
+
+menuBtn.addEventListener("click", function () {
+  navLinks.classList.add("menu__animated");
+  const isOpen = navLinks.classList.toggle("open__menu");
+
+  this.classList.toggle("close__menu");
+  this.setAttribute("aria-expanded", isOpen);
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 720) {
+    navLinks.classList.remove("menu__animated");
+  }
+});
